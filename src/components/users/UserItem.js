d5c05c835c4c6a6f5369f2d7bb6fde77adc782b8
Erik Svensson <eosvensson@gmail.com>
2020-05-07 13:44:42 +0200

    Refactoring into functional components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,24 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-    state ={
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-    }
-    render() {
-        const {login, avatar_url, html_url} = this.props.user; //Destructuring
-        return (
-            <div className='card text-center'>
-                <img src={avatar_url} alt='Not found' className='round-img' style={{width: '60px'}}></img>
-                <h3>{login}</h3>
-                <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-                </div>
+const UserItem = ({user: {login, avatar_url, html_url}}) => { //const {login, avatar_url, html_url} = props.user Functionality moved up here
+    //const {login, avatar_url, html_url} = props.user; //Destructuring
+    return (
+        <div className='card text-center'>
+            <img src={avatar_url} alt='Not found' className='round-img' style={{width: '60px'}}/>
+            <h3>{login}</h3>
+            <div>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
             </div>
-        );
-    }
+        </div>
+    );
+}
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
 }
 
 export default UserItem;
