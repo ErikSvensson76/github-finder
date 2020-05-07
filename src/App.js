e004710c0379926d3107769e1b8bbf59901e3b5f
Erik Svensson <eosvensson@gmail.com>
2020-05-07 15:41:52 +0200

    Calling the Github api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, {Fragment, Component} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Navbar from "./components/layout/NavBar";
 import Users from "./components/users/Users";
+import axios from 'axios';
 
 class App extends Component {
+    state = {
+        users: [],
+        loading: false
+    }
+    async componentDidMount() {
+        this.setState({loading: true})
+        const response = await axios.get(`https://api.github.com/users?
+        client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+        client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        this.setState({users: response.data, loading: false})
+    }
+
     render(){
         return (
-            <Fragment className='App'>
+            <div className='App'>
                 <Navbar />
                 <div className='container'>
-                    <Users />
+                    <Users loading={this.state.loading} users={this.state.users}/>
                 </div>
-            </Fragment>
+            </div>
         );
     }
 }
