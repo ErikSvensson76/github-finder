d8e337b245a04cc301b450bab109728c0a4af4e2
Erik Svensson <eosvensson@gmail.com>
2020-05-08 11:07:53 +0200

    Added PropTypes to App.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,17 @@ import Navbar from "./components/layout/NavBar";
 import Users from "./components/users/Users";
 import axios from 'axios';
 import Search from "./components/users/Search";
+import PropTypes from 'prop-types';
 
 class App extends Component {
     state = {
         users: [],
         loading: false
     }
-/*
-    async componentDidMount() {
-        this.setState({loading: true})
-        const response = await axios.get(`https://api.github.com/users?
-        client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-        client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-        this.setState({users: response.data, loading: false})
-    }
-*/
 
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired
+    }
 
 
     searchUsers = async text =>{
